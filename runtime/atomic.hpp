e2e6f038e15f10e94937780ca4627557428eebfa
LizBing <lizbing@relight.team>
2023-08-14 22:58:17 +0800

    modify 'atomic'

--- runtime/atomic.hpp
+++ runtime/atomic.hpp
@@ -2,48 +2,43 @@
 #define RUNTIME_ATOMIC_
 
 #include "memory/allStatic.hpp"
+#include <atomic>
+#include <stdatomic.h>
 
 // using the same memory orders as C11
 struct Atomic : AllStatic {
-    template<class T>
-    static T load(T volatile* p, memory_order order = memory_order_seq_cst); 
-
-    template<class T, class U>
-    static void store(T volatile* p, U des, memory_order order = memory_order_seq_cst); 
-
-    template<class T, class U>
-    static T fetchAndAdd(T volatile* p, U n, memory_order order = memory_order_seq_cst) {
-        return atomic_fetch_add_explicit((T _Atomic*)p, n, order);
-    }
+    enum MemoryOrder {
+        relaxed,
+        consume,
+        acquire,
+        release,
+        acq_rel,
+        seq_cst,
+    };
 
     template<class T>
-    static void inc(T volatile* p, memory_order order = memory_order_seq_cst) {
-        fetchAndAdd(p, 1, order);
-    }
-
-    template<class T>
-    static void dec(T volatile* p, memory_order order = memory_order_seq_cst) {
-        fetchAndAdd(p, -1, order);
-    }
+    static T load(T volatile* p, MemoryOrder order = seq_cst); 
 
     template<class T, class U>
-    static T xchg(T volatile* p, U des, memory_order order = memory_order_seq_cst) {
-        return atomic_exchange_explicit((T _Atomic*)p, des, order);
-    }
+    static void store(T volatile* p, U des, MemoryOrder order = seq_cst); 
+
+    template<class T, class U>
+    static T fetchAndAdd(T volatile* p, U n, MemoryOrder order = seq_cst); 
+
+    template<class T>
+    static void inc(T volatile* p, MemoryOrder order = seq_cst) { fetchAndAdd(p, 1, order); }
+
+    template<class T>
+    static void dec(T volatile* p, MemoryOrder order = seq_cst) { fetchAndAdd(p, -1, order); }
+
+    template<class T, class U>
+    static T xchg(T volatile* p, U des, MemoryOrder order = seq_cst); 
 
     template<class T, class U, class V>
-    static T cmpxchg_strong(T volatile* p, U exp, V des, memory_order order = memory_order_seq_cst) {
-        T ret_val = exp;
-        atomic_compare_exchange_strong_explicit((T _Atomic*)p, &ret_val, des);
-        return ret_val;
-    }
+    static T cmpxchg_strong(T volatile* p, U exp, V des, MemoryOrder order = seq_cst); 
     
     template<class T, class U, class V>
-    static T cmpxchg_weak(T volatile* p, U exp, V des, memory_order order = memory_order_seq_cst) {
-        T ret_val = exp;
-        atomic_compare_exchange_weak_explicit((T _Atomic*)p, &ret_val, des);
-        return ret_val;
-    }
+    static T cmpxchg_weak(T volatile* p, U exp, V des, MemoryOrder order = seq_cst); 
 };
 
 #endif
